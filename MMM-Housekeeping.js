dcf4fc9dddbd40922ca972d6a6b155ecd94c56a4
Jørgen Ellingsen <jorgen@ellera.no>
2023-01-20 14:30:00 +0100

    v0.1 test

---
+++ MMM-Housekeeping.js
@@ -0,0 +1,17 @@
+
+Module.register("MMM-Housekeeping", {
+
+    // Override dom generator.
+    getDom: function () {
+        const d = Date.parse('20 Jan 2022 00:00:00 GMT');
+        const w =  Math.round((d - new Date()) / (7 * 24 * 60 * 60 * 1000));
+        const c = [
+            'Ellera',
+            'Imara',
+            'CSO Partner'
+        ];
+        var wrapper = document.createElement("div");
+        wrapper.innerHTML = c[w%3] + ' is on duty this week';
+        return wrapper;
+    },
+});
