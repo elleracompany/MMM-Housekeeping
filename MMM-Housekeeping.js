f7933655f8c129079d29fdfa3e361a713b08bf12
Jørgen Ellingsen <jorgen@ellera.no>
2023-01-20 14:31:29 +0100

    Middle of the week

--- MMM-Housekeeping.js
+++ MMM-Housekeeping.js
@@ -3,7 +3,7 @@ Module.register("MMM-Housekeeping", {
 
     // Override dom generator.
     getDom: function () {
-        const d = Date.parse('20 Jan 2022 00:00:00 GMT');
+        const d = Date.parse('18 Jan 2022 00:00:00 GMT');
         const w =  Math.round((d - new Date()) / (7 * 24 * 60 * 60 * 1000));
         const c = [
             'Ellera',
