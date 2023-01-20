ea36a56f085bc26c4912d9ee3282b7761978ca1c
Jørgen Ellingsen <jorgen@ellera.no>
2023-01-20 14:35:16 +0100

    Test

--- MMM-Housekeeping.js
+++ MMM-Housekeeping.js
@@ -11,7 +11,7 @@ Module.register("MMM-Housekeeping", {
             'CSO Partner'
         ];
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = c[w%3] + ' is on duty this week';
+        wrapper.innerHTML = w;
         return wrapper;
     },
 });
