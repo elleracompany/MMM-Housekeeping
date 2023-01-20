c7e4e608844c7fe77421e75072374f59e779a3da
Jørgen Ellingsen <jorgen@ellera.no>
2023-01-20 14:38:36 +0100

    Wrong year

--- MMM-Housekeeping.js
+++ MMM-Housekeeping.js
@@ -3,7 +3,7 @@ Module.register("MMM-Housekeeping", {
 
     // Override dom generator.
     getDom: function () {
-        const d = Date.parse('18 Jan 2022 00:00:00 GMT');
+        const d = Date.parse('18 Jan 2023 00:00:00 GMT');
         const w =  Math.round((new Date() - d) / (7 * 24 * 60 * 60 * 1000));
         const c = [
             'Ellera',
