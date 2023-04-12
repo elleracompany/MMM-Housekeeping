01d6a276ef5c7b256f262961b1568e3dfc234b30
PerE_Ellera <pererik@ellera.no>
2023-04-12 12:10:31 +0200

    Did not work

--- MMM-Housekeeping.js
+++ MMM-Housekeeping.js
@@ -2,8 +2,7 @@
 Module.register("MMM-Housekeeping", {
 
     // Override dom generator.
-    getDom: window.setInterval(
-      function () {
+    getDom: function () {
         const d = Date.parse('18 Jan 2023 00:00:00 GMT');
         const w =  Math.round((new Date() - d) / (7 * 24 * 60 * 60 * 1000));
         const c = [
@@ -14,5 +13,5 @@ Module.register("MMM-Housekeeping", {
         var wrapper = document.createElement("div");
         wrapper.innerHTML = c[w%3] + ' har ryddeansvaret denne uken';
         return wrapper;
-    },60000),
+    },
 });
